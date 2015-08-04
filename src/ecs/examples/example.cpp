5d821056014080934a236a0a35f0e8cfeaa7d921
robin <robgro@telia.com>
2015-08-05 01:26:57 +0200

    License stuff

--- src/ecs/examples/example.cpp
+++ src/ecs/examples/example.cpp
@@ -1,3 +1,20 @@
+/// --------------------------------------------------------------------------
+/// OpenEcs - A fast, typesafe, C++11, configurable, header only, ECS
+/// Copyright (C) 2015  Robin Grönberg
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <iostream>
 #include "ecs/ecs.h"
 
