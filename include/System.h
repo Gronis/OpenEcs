14306be02404c6a24fd277e87026e50e12eb6b80
robin <robgro@telia.com>
2015-11-29 12:08:10 +0100

    Moved SystemManager and System to separate files. System is no longer an inner class of SystemManager.

---
+++ include/System.h
@@ -0,0 +1,14 @@
+//
+// Created by Robin Grönberg on 29/11/15.
+//
+
+#ifndef OPENECS_SYSTEM_H
+#define OPENECS_SYSTEM_H
+
+
+class System {
+
+};
+
+
+#endif //OPENECS_SYSTEM_H
---
+++ include/System.inl
@@ -0,0 +1,5 @@
+//
+// Created by Robin Grönberg on 29/11/15.
+//
+
+#include "System.h"
