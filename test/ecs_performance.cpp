a501c67475dd3a7928a2352b272f79866cf4c2c4
robin <robgro@telia.com>
2015-12-01 20:50:26 +0100

    Added tests for checking performance impact when different entities are spread out in memory.

--- test/ecs_performance.cpp
+++ test/ecs_performance.cpp
@@ -1,4 +1,3 @@
-
 /// --------------------------------------------------------------------------
 /// Copyright (C) 2015  Robin Grönberg
 ///
@@ -25,221 +24,254 @@
 using namespace ecs;
 
 namespace {
-    struct Wheels {
-        int value;
-    };
+struct Wheels {
+  int value;
+};
 
-    struct Door {
-        int value;
-    };
+struct Door {
+  int value;
+};
 
-    struct Hat{
-        int i;
-    };
+struct Hat {
+  int i;
+};
 
-    struct Clothes{
-        float i;
-    };
+struct Clothes {
+  int i[16];
+};
 
-    struct Car : EntityAlias<Wheels> {
-    };
+struct Car: EntityAlias<Wheels> {
+};
 
-    // Used to make sure the comparator does not optimize away my for-loop
-    struct BaseFoo{
-        virtual void bar() = 0;
-        bool always_false = false;
-    };
+// Used to make sure the comparator does not optimize away my for-loop
+struct BaseFoo {
+  virtual void bar() = 0;
+  bool always_false = false;
+};
 
-    struct Foo : BaseFoo{
-        virtual void bar() override{
+struct Foo: BaseFoo {
+  virtual void bar() override {
 
-        }
-    };
+  }
+};
 }
 
 class Timer {
-public:
-    Timer(){ restart(); }
-    ~Timer(){ std::cout << "Time elapsed: " << elapsed() << std::endl << std::endl;}
+ public:
+  Timer() { restart(); }
+  ~Timer() { std::cout << "Time elapsed: " << elapsed() << std::endl << std::endl; }
 
-    void restart() { _start = std::chrono::system_clock::now(); }
-    double elapsed() { return std::chrono::duration<double>(std::chrono::system_clock::now() - _start).count(); }
-private:
-    std::chrono::time_point<std::chrono::system_clock> _start;
+  void restart() { _start = std::chrono::system_clock::now(); }
+  double elapsed() { return std::chrono::duration<double>(std::chrono::system_clock::now() - _start).count(); }
+ private:
+  std::chrono::time_point<std::chrono::system_clock> _start;
 };
 
 TEST_CASE("TestEntityCreation") {
-    int count = 10000000;
-    EntityManager em;
+  int count = 10000000;
+  EntityManager em;
 
-    std::cout << "Creating " << count << " entities using create()" << std::endl;
-    Timer t;
-    for (int i = 0; i < count; ++i) {
-        em.create();
-    }
-    REQUIRE(em.count() == count);
+  std::cout << "Creating " << count << " entities using create()" << std::endl;
+  Timer t;
+  for (int i = 0; i < count; ++i) {
+    em.create();
+  }
+  REQUIRE(em.count() == count);
 }
 
 TEST_CASE("TestEntityCreationMany") {
-    int count = 10000000;
-    EntityManager em;
+  int count = 10000000;
+  EntityManager em;
 
-    std::cout << "Creating " << count << " entities using create(int)" << std::endl;
-    Timer t;
-    auto new_entities = em.create(count);
-    REQUIRE(em.count() == count);
+  std::cout << "Creating " << count << " entities using create(int)" << std::endl;
+  Timer t;
+  auto new_entities = em.create(count);
+  REQUIRE(em.count() == count);
 }
 
 TEST_CASE("TestEntityDestruction") {
-    int count = 10000000;
-    EntityManager em;
+  int count = 10000000;
+  EntityManager em;
+  auto entities = em.create(count);
+  {
+    std::cout << "Destroying " << count << " entities" << std::endl;
+    Timer t;
+    for (size_t i = 0; i < entities.size(); ++i) {
+      entities[i].destroy();
+    }
+    REQUIRE(em.count() == 0);
+  }
+  {
+    std::cout << "Recreating after destroying " << count << " entities" << std::endl;
+    Timer t;
     auto entities = em.create(count);
-    {
-        std::cout << "Destroying " << count << " entities" << std::endl;
-        Timer t;
-        for (size_t i = 0; i < entities.size(); ++i) {
-            entities[i].destroy();
-        }
-        REQUIRE(em.count() == 0);
-    }
-    {
-        std::cout << "Recreating after destroying " << count << " entities" << std::endl;
-        Timer t;
-        auto entities = em.create(count);
-    }
+  }
 }
 
 SCENARIO("TestEntityIteration") {
-    const int count = 10000000;
-    EntityManager entities;
-    for (int i = 0; i < count; i++) {
-        entities.create_with<Wheels, Door>();
-    }
+  const int count = 10000000;
+  EntityManager entities;
+  for (int i = 0; i < count; i++) {
+    entities.create_with<Wheels, Door>();
+  }
 
-    WHEN("Iterating using normal for-loop"){
-        std::cout << "Iterating over " << count << " using normal for loop" << std::endl;
-        BaseFoo* foo = new Foo();
-        {
-            Timer t;
-            for (int i = 0; i < count; ++i) {
-                //we don't want to call dummy function.
-                if(foo->always_false){
-                    //useless function call to make sure the loop is not optimized away.
-                    foo->bar();
-                }
-            }
+  WHEN("Iterating using normal for-loop") {
+    std::cout << "Iterating over " << count << " using normal for loop" << std::endl;
+    BaseFoo *foo = new Foo();
+    {
+      Timer t;
+      for (int i = 0; i < count; ++i) {
+        //we don't want to call dummy function.
+        if (foo->always_false) {
+          //useless function call to make sure the loop is not optimized away.
+          foo->bar();
         }
+      }
     }
+  }
 
-    WHEN("Iterating manually with iterator"){
-        std::cout << "Iterating over " << count << " using iterator manually" << std::endl;
-        Timer t;
-        auto it = entities.with<Wheels>().begin();
-        auto end = entities.with<Wheels>().end();
-        for (; it != end; ++it) {
-            (void)it;
-        }
+  WHEN("Iterating manually with iterator") {
+    std::cout << "Iterating over " << count << " using iterator manually" << std::endl;
+    Timer t;
+    auto it = entities.with<Wheels>().begin();
+    auto end = entities.with<Wheels>().end();
+    for (; it != end; ++it) {
+      (void) it;
     }
+  }
 
-    WHEN("Iterating using with") {
-        std::cout << "Iterating over " << count << " using with for-loop without unpacking" << std::endl;
-        Timer t;
-        for (auto e : entities.with<Wheels>()) {
-            (void)e;
-        }
+  WHEN("Iterating using with") {
+    std::cout << "Iterating over " << count << " using with for-loop without unpacking" << std::endl;
+    Timer t;
+    for (auto e : entities.with<Wheels>()) {
+      (void) e;
     }
+  }
 
-    WHEN("Iterating using with") {
-        std::cout << "Iterating over " << count << " using with for-loop unpacking one component" << std::endl;
-        Timer t;
-        for (auto e : entities.with<Wheels>()) {
-            e.get<Wheels>();
-        }
+  WHEN("Iterating using with") {
+    std::cout << "Iterating over " << count << " using with for-loop unpacking one component" << std::endl;
+    Timer t;
+    for (auto e : entities.with<Wheels>()) {
+      e.get<Wheels>();
     }
+  }
 
-    WHEN("Iterating using with") {
-        std::cout << "Iterating over " << count << " using with lambda unpacking one component" << std::endl;
-        Timer t;
-        entities.with([](Wheels& wheels){
-            (void)wheels;
-        } );
-    }
+  WHEN("Iterating using with") {
+    std::cout << "Iterating over " << count << " using with lambda unpacking one component" << std::endl;
+    Timer t;
+    entities.with([](Wheels &wheels) {
+      (void) wheels;
+    });
+  }
 
-    WHEN("Iterating using with") {
-        std::cout << "Iterating over " << count << " using with for-loop unpacking two components" << std::endl;
-        Timer t;
-        for (auto e : entities.with<Wheels, Door>()) {
-            e.get<Wheels>();
-            e.get<Door>();
-        }
+  WHEN("Iterating using with") {
+    std::cout << "Iterating over " << count << " using with for-loop unpacking two components" << std::endl;
+    Timer t;
+    for (auto e : entities.with<Wheels, Door>()) {
+      e.get<Wheels>();
+      e.get<Door>();
     }
+  }
 
-    WHEN("Iterating using with") {
-        std::cout << "Iterating over " << count << " using with lambda unpacking two components" << std::endl;
-        Timer t;
-        entities.with([](Wheels& wheels, Door& door){
-            (void)wheels;
-            (void)door;
-        } );
-    }
+  WHEN("Iterating using with") {
+    std::cout << "Iterating over " << count << " using with lambda unpacking two components" << std::endl;
+    Timer t;
+    entities.with([](Wheels &wheels, Door &door) {
+      (void) wheels;
+      (void) door;
+    });
+  }
 
-    WHEN("Iterating using fetch_every") {
-        std::cout << "Iterating over " << count << " using fetch_every for-loop" << std::endl;
-        Timer t;
-        for (auto e : entities.fetch_every<Car>()) {
-            (void) e;
-        }
+  WHEN("Iterating using fetch_every") {
+    std::cout << "Iterating over " << count << " using fetch_every for-loop" << std::endl;
+    Timer t;
+    for (auto e : entities.fetch_every<Car>()) {
+      (void) e;
     }
+  }
 
-    WHEN("Iterating using fetch_every") {
-        std::cout << "Iterating over " << count << " using fetch_every lambda" << std::endl;
-        Timer t;
-        entities.fetch_every([](Car& car){
-            (void)car;
-        } );
-    }
+  WHEN("Iterating using fetch_every") {
+    std::cout << "Iterating over " << count << " using fetch_every lambda" << std::endl;
+    Timer t;
+    entities.fetch_every([](Car &car) {
+      (void) car;
+    });
+  }
 }
 
-SCENARIO("TestEntityIterationForDifferentEntities") {
-    int count = 10000000;
-    EntityManager entities;
-    for (int i = 0; i < count / 2; i++) {
-        entities.create_with<Wheels, Door>(10,10);
-        entities.create_with<Wheels, Hat>(10,10);
-    }
+SCENARIO("TestEntityIterationForContinousMemory hard code") {
+  const int count = 10000000;
+  Door* doors = new Door[count];
 
-    WHEN("Iterating using with<Wheels, Door>()") {
-        std::cout << "Iterating over " << count << " using with Doors and Wheels  ("
-                  << entities.with<Wheels, Door>().count() << ")" << std::endl;
-        {
-            Timer t;
-            entities.with([](Wheels& wheels, Door& door){
-                (void)wheels;
-                (void)door;
-            } );
+  WHEN("Iterating over entities with doors (hard code)") {
+    std::cout << "Iterating over " << count << " using with Doors continous in memory (hard code)" << std::endl;
+    BaseFoo *foo = new Foo();
+    {
+      Timer t;
+      for (int i = 0; i < count; ++i) {
+        if(foo->always_false){
+          foo->bar();
         }
+        (void)doors[i].value;
+      }
     }
+  }
+}
 
-    WHEN("Iterating using with<Wheels, Hat>()") {
-        std::cout << "Iterating over " << count << " using with Wheels and Hat ("
-                  << entities.with<Wheels, Hat>().count() << ")" << std::endl;
-        {
-            Timer t;
-            entities.with([](Hat& hat, Wheels& wheels){
-                (void)hat;
-                (void)wheels;
-            } );
-        }
-    }
+SCENARIO("TestEntityIterationForSplittedMemory hard code") {
+  const int count = 10000000;
+  Door* doors = new Door[count * 100];
 
-    WHEN("Iterating using with<Clothes>()") {
-        std::cout << "Iterating over " << count << " using with Clothes ("
-                  << entities.with<Clothes>().count() << ")" << std::endl;
-        {
-            Timer t;
-            entities.with([](Clothes& clothes){
-                (void)clothes;
-            } );
+  WHEN("Iterating over entities with doors (hard code)") {
+    std::cout << "Iterating over " << count << " using with Doors split in memory (hard code)" << std::endl;
+    BaseFoo *foo = new Foo();
+    {
+      Timer t;
+      for (int i = 0; i < count * 100; i+=100) {
+        if(foo->always_false){
+          foo->bar();
         }
+        (void)doors[i].value;
+      }
     }
+  }
+}
+
+SCENARIO("TestEntityIterationForContinousMemory") {
+  int count = 10000000;
+  EntityManager entities;
+
+  for (int i = 0; i < count / 16; ++i) {
+    for (int j = 0; j < 15; ++j) {
+      entities.create_with<Wheels>();
+    }
+    entities.create_with<Clothes>();
+  }
+
+  WHEN("Iterating over entities with doors") {
+    std::cout << "Iterating over " << count << " using with Doors continous in memory" << std::endl;
+    {
+      Timer t;
+      entities.with([](Clothes &clothes) { (void) clothes.i; });
+    }
+  }
+}
+
+SCENARIO("TestEntityIterationForSplittedMemory") {
+  int count = 10000000;
+  EntityManager entities;
+  for (int i = 0; i < count / 16; ++i) {
+    for (int j = 0; j < 15; ++j) {
+      entities.create().add<Wheels>();
+    }
+    entities.create().add<Clothes>();
+  }
+
+  WHEN("Iterating over entities with doors") {
+    std::cout << "Iterating over " << count << " using with Doors, split in memory" << std::endl;
+    {
+      Timer t;
+      entities.with([](Clothes &clothes) { (void) clothes.i; });
+    }
+  }
 }
